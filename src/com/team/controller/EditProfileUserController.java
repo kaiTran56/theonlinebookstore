dc59549db7dfc0760b517a080444c595a1e2b3f7
TranKai <quyettran057@gmail.com>
2020-12-12 12:18:53 +0700

    new updated code!

--- src/com/team/controller/EditProfileUserController.java
+++ src/com/team/controller/EditProfileUserController.java
@@ -49,14 +49,4 @@ public class EditProfileUserController extends HttpServlet {
 
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
-	 *      response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
-	}
-
 }
