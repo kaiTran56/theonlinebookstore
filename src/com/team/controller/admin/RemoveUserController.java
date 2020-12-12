5f33ecb2b2f20000463d906cf0fc044e58a99f5d
TranKai <quyettran057@gmail.com>
2020-12-12 13:34:15 +0700

    new updated code!

--- src/com/team/controller/admin/RemoveUserController.java
+++ src/com/team/controller/admin/RemoveUserController.java
@@ -15,16 +15,22 @@ import com.team.dao.impl.UserDaoImpl;
 
 public class RemoveUserController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-   
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		doRemoveUser(request, response);
+
+	}
+
+	protected void doRemoveUser(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		String email = (String) request.getParameter("user-email");
 		new UserDaoImpl().delete(email);
 		request.getRequestDispatcher("/admin/list-user").forward(request, response);
 	}
 
-	
 }
